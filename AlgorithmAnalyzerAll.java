954f4179eb30a1c064a1ee0d9c03109bd0ac7829
nhjayasundara-web <n.h.jayasundara@gmail.com>
2025-11-15 23:14:26 +0530

    commit

---
+++ AlgorithmAnalyzerAll.java
@@ -0,0 +1,11 @@
+public class AlgorithmAnalyzerAll {
+    public static void main(String[] args) {
+        LinearSearchTimer.main(args);
+        System.out.println();
+        BinarySearchTimer.main(args);
+        System.out.println();
+        BubbleSortTimer.main(args);
+        System.out.println();
+        QuickSortTimer.main(args);
+    }
+}
